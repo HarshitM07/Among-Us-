88edb946daeffb57bca496ead98de69c433e92d9
HarshitM07 <128212606+HarshitM07@users.noreply.github.com>
2024-03-19 00:47:00 +0530

    Initialcommit

---
+++ android/app/src/main/kotlin/com/example/among_us2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.among_us2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset(
+            'assets/login.png',
+            fit: BoxFit.cover,
+          ),
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: EdgeInsets.all(30.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'LOGIN HERE',
+                      style: TextStyle(
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 50.0),
+                    TextField(
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        hintText: '@nith.ac.in',
+                        hintStyle: const TextStyle(
+                            color: Color.fromARGB(255, 188, 210, 228)),
+                        //prefixText: '@nith.ac.in',
+                        //prefixStyle: TextStyle(color: Colors.white),
+                        filled: true,
+                        fillColor:
+                            Color.fromARGB(255, 28, 11, 97).withOpacity(0.5),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 30.0),
+                    TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Enter your Password',
+                        hintStyle: const TextStyle(
+                            color: Color.fromARGB(255, 188, 210, 228)),
+                        filled: true,
+                        fillColor:
+                            Color.fromARGB(255, 28, 11, 97).withOpacity(0.5),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20.0),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Perform login logic here
+                      },
+                      child: Text('Submit'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
