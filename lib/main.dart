6b5e18f078bde1bd3ac1a47c93ab7813b68b7839
HarshitM07 <128212606+HarshitM07@users.noreply.github.com>
2024-03-19 19:49:36 +0530

    minor changes in frontend

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,125 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Login App',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: LoginPage(),
+//     );
+//   }
+// }
+
+// class LoginPage extends StatefulWidget {
+//   @override
+//   _LoginPageState createState() => _LoginPageState();
+// }
+
+// class _LoginPageState extends State<LoginPage> {
+//   final TextEditingController _emailController = TextEditingController();
+//   final TextEditingController _passwordController = TextEditingController();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         fit: StackFit.expand,
+//         children: [
+//           Image.asset(
+//             'assets/login.png',
+//             fit: BoxFit.cover,
+//           ),
+//           Center(
+//             child: SingleChildScrollView(
+//               child: Padding(
+//                 padding: EdgeInsets.all(30.0),
+//                 child: Column(
+//                   mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                   children: [
+//                     const Text(
+//                       'LOGIN HERE',
+//                       style: TextStyle(
+//                         fontSize: 28.0,
+//                         fontWeight: FontWeight.bold,
+//                         color: Colors.white,
+//                       ),
+//                     ),
+//                     SizedBox(height: 45.0),
+//                     const Text(
+//                       'Enter your ID',
+//                       style: TextStyle(
+//                         fontSize: 18.0,
+//                         fontWeight: FontWeight.normal,
+//                         color: Colors.white,
+//                       ),
+//                     ),
+//                     SizedBox(height: 5.0),
+//                     TextField(
+//                       controller: _emailController,
+//                       decoration: InputDecoration(
+//                         hintText: 'e.g., @nith.ac.in',
+//                         hintStyle: const TextStyle(
+//                             color: Color.fromARGB(255, 188, 210, 228)),
+//                         //prefixText: '@nith.ac.in',
+//                         //prefixStyle: TextStyle(color: Colors.white),
+//                         filled: true,
+//                         fillColor:
+//                             Color.fromARGB(255, 28, 11, 97).withOpacity(0.5),
+//                         border: OutlineInputBorder(
+//                           borderRadius: BorderRadius.circular(10.0),
+//                         ),
+//                       ),
+//                     ),
+//                     SizedBox(height: 30.0),
+//                     const Text(
+//                       'Enter password',
+//                       style: TextStyle(
+//                         fontSize: 18.0,
+//                         fontWeight: FontWeight.normal,
+//                         color: Colors.white,
+//                       ),
+//                     ),
+//                     SizedBox(height: 5.0),
+//                     TextField(
+//                       controller: _passwordController,
+//                       obscureText: true,
+//                       decoration: InputDecoration(
+//                         hintText: 'Enter your Password',
+//                         hintStyle: const TextStyle(
+//                             color: Color.fromARGB(255, 188, 210, 228)),
+//                         filled: true,
+//                         fillColor:
+//                             Color.fromARGB(255, 28, 11, 97).withOpacity(0.5),
+//                         border: OutlineInputBorder(
+//                           borderRadius: BorderRadius.circular(10.0),
+//                         ),
+//                       ),
+//                     ),
+//                     SizedBox(height: 40.0),
+//                     ElevatedButton(
+//                       onPressed: () {
+//                         // Perform login logic here
+//                       },
+//                       child: Text('Submit'),
+//                     ),
+//                   ],
+//                 ),
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -41,7 +163,7 @@ class _LoginPageState extends State<LoginPage> {
               child: Padding(
                 padding: EdgeInsets.all(30.0),
                 child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     const Text(
                       'LOGIN HERE',
@@ -51,24 +173,25 @@ class _LoginPageState extends State<LoginPage> {
                         color: Colors.white,
                       ),
                     ),
-                    SizedBox(height: 45.0),
-                    const Text(
-                      'Enter your ID',
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.white,
+                    SizedBox(height: 60.0),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        'Enter your ID',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                    SizedBox(height: 5.0),
+                    SizedBox(height: 8.0),
                     TextField(
                       controller: _emailController,
                       decoration: InputDecoration(
                         hintText: 'e.g., @nith.ac.in',
                         hintStyle: const TextStyle(
                             color: Color.fromARGB(255, 188, 210, 228)),
-                        //prefixText: '@nith.ac.in',
-                        //prefixStyle: TextStyle(color: Colors.white),
                         filled: true,
                         fillColor:
                             Color.fromARGB(255, 28, 11, 97).withOpacity(0.5),
@@ -78,15 +201,18 @@ class _LoginPageState extends State<LoginPage> {
                       ),
                     ),
                     SizedBox(height: 30.0),
-                    const Text(
-                      'Enter password',
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.normal,
-                        color: Colors.white,
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: const Text(
+                        'Enter password',
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          fontWeight: FontWeight.normal,
+                          color: Colors.white,
+                        ),
                       ),
                     ),
-                    SizedBox(height: 5.0),
+                    SizedBox(height: 8.0),
                     TextField(
                       controller: _passwordController,
                       obscureText: true,
