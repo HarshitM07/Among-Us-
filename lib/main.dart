e38202b1d2d632decafecfc3cf3f9a9354c2fe07
HarshitM07 <128212606+HarshitM07@users.noreply.github.com>
2024-03-20 19:15:26 +0530

    2nd page completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,5 @@
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Login App',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: LoginPage(),
-//     );
-//   }
-// }
-
-// class LoginPage extends StatefulWidget {
-//   @override
-//   _LoginPageState createState() => _LoginPageState();
-// }
-
-// class _LoginPageState extends State<LoginPage> {
-//   final TextEditingController _emailController = TextEditingController();
-//   final TextEditingController _passwordController = TextEditingController();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Stack(
-//         fit: StackFit.expand,
-//         children: [
-//           Image.asset(
-//             'assets/login.png',
-//             fit: BoxFit.cover,
-//           ),
-//           Center(
-//             child: SingleChildScrollView(
-//               child: Padding(
-//                 padding: EdgeInsets.all(30.0),
-//                 child: Column(
-//                   mainAxisAlignment: MainAxisAlignment.spaceAround,
-//                   children: [
-//                     const Text(
-//                       'LOGIN HERE',
-//                       style: TextStyle(
-//                         fontSize: 28.0,
-//                         fontWeight: FontWeight.bold,
-//                         color: Colors.white,
-//                       ),
-//                     ),
-//                     SizedBox(height: 45.0),
-//                     const Text(
-//                       'Enter your ID',
-//                       style: TextStyle(
-//                         fontSize: 18.0,
-//                         fontWeight: FontWeight.normal,
-//                         color: Colors.white,
-//                       ),
-//                     ),
-//                     SizedBox(height: 5.0),
-//                     TextField(
-//                       controller: _emailController,
-//                       decoration: InputDecoration(
-//                         hintText: 'e.g., @nith.ac.in',
-//                         hintStyle: const TextStyle(
-//                             color: Color.fromARGB(255, 188, 210, 228)),
-//                         //prefixText: '@nith.ac.in',
-//                         //prefixStyle: TextStyle(color: Colors.white),
-//                         filled: true,
-//                         fillColor:
-//                             Color.fromARGB(255, 28, 11, 97).withOpacity(0.5),
-//                         border: OutlineInputBorder(
-//                           borderRadius: BorderRadius.circular(10.0),
-//                         ),
-//                       ),
-//                     ),
-//                     SizedBox(height: 30.0),
-//                     const Text(
-//                       'Enter password',
-//                       style: TextStyle(
-//                         fontSize: 18.0,
-//                         fontWeight: FontWeight.normal,
-//                         color: Colors.white,
-//                       ),
-//                     ),
-//                     SizedBox(height: 5.0),
-//                     TextField(
-//                       controller: _passwordController,
-//                       obscureText: true,
-//                       decoration: InputDecoration(
-//                         hintText: 'Enter your Password',
-//                         hintStyle: const TextStyle(
-//                             color: Color.fromARGB(255, 188, 210, 228)),
-//                         filled: true,
-//                         fillColor:
-//                             Color.fromARGB(255, 28, 11, 97).withOpacity(0.5),
-//                         border: OutlineInputBorder(
-//                           borderRadius: BorderRadius.circular(10.0),
-//                         ),
-//                       ),
-//                     ),
-//                     SizedBox(height: 40.0),
-//                     ElevatedButton(
-//                       onPressed: () {
-//                         // Perform login logic here
-//                       },
-//                       child: Text('Submit'),
-//                     ),
-//                   ],
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
+import 'package:among_us2/nameYourTeam.dart';
 
 void main() {
   runApp(MyApp());
@@ -217,7 +96,7 @@ class _LoginPageState extends State<LoginPage> {
             ),
           ),
           Positioned(
-            top: 60,
+            top: 40,
             left: 30,
             child: Image.asset(
               'assets/Group.png',
@@ -289,6 +168,11 @@ class _LoginPageState extends State<LoginPage> {
                     ElevatedButton(
                       onPressed: () {
                         // Perform login logic here
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => NameYourTeam()),
+                        );
                       },
                       style: ButtonStyle(
                         backgroundColor: MaterialStateProperty.all<Color>(
@@ -296,6 +180,13 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                         fixedSize: MaterialStateProperty.all<Size>(
                           Size(200, 50), // Change button size here
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(
+                                10), // Adjust the radius as needed
+                          ),
                         ),
                       ),
                       child: Text(
---
+++ lib/nameYourTeam.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class NameYourTeam extends StatefulWidget {
+  @override
+  _NameYourTeamState createState() => _NameYourTeamState();
+}
+
+class _NameYourTeamState extends State<NameYourTeam> {
+  final TextEditingController _teamNameController = TextEditingController();
+  final TextEditingController _teamCodeController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // appBar: AppBar(
+        //   title: Text('Name Your Crew'),
+        // ),
+        body: Stack(fit: StackFit.expand, children: [
+      Image.asset(
+        'assets/NameYourTeam (1).png',
+        fit: BoxFit.cover,
+      ),
+      Padding(
+        padding: EdgeInsets.all(45.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Positioned(
+              top: 20,
+              left: 0,
+              child: Container(
+                width: 257, // Set the desired width
+                height: 33, // Set the desired height
+                child: Image.asset(
+                  'assets/NAME YOUR CREW.png',
+                  fit: BoxFit.cover, // Adjust image fit as needed
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Positioned(
+              top: 20,
+              left: 0,
+              child: Container(
+                width: 162, // Set the desired width
+                height: 149, // Set the desired height
+                child: Image.asset(
+                  'assets/Group 5.png',
+                  fit: BoxFit.cover, // Adjust image fit as needed
+                ),
+              ),
+            ),
+            SizedBox(height: 50),
+            TextField(
+              controller: _teamNameController,
+              decoration: InputDecoration(
+                hintText: 'Team Name',
+                hintStyle:
+                    const TextStyle(color: Color.fromARGB(255, 10, 15, 19)),
+                filled: true,
+                fillColor: Color.fromRGBO(228, 223, 174, 1),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                // border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 35),
+            TextField(
+              controller: _teamCodeController,
+              decoration: InputDecoration(
+                hintText: 'Team Code',
+                hintStyle:
+                    const TextStyle(color: Color.fromARGB(255, 10, 15, 19)),
+                filled: true,
+                fillColor: Color.fromRGBO(228, 223, 174, 1),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                // border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 40),
+            ElevatedButton(
+              onPressed: () {
+                // Implement your ready to play logic here
+              },
+              child: Text('Ready to play'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromRGBO(75, 62, 26, 1),
+                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 16),
+                shape: RoundedRectangleBorder(
+                  borderRadius:
+                      BorderRadius.circular(10), // Adjust the radius as needed
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ]));
+  }
+}
